69af34cc037c665142a3f4f8cec0ab5d558c425c
chenchenchen <117@kunkun.com>
2025-02-28 18:04:35 +0800

    ...

--- main_run.py
+++ main_run.py
@@ -3,8 +3,9 @@ import os
 sim = 'cos'
 lmd = 0.3
 drop = 0.9
-dataset = "steam"
+dataset = ["steam", "lfm1b-traks", "ml-1m"]
 lmd_tf = 0.5
-os.system("python run_seq.py --dataset={} --train_batch_size=256 "
+for data in dataset:
+    os.system("python run_seq.py --dataset={} --train_batch_size=256 "
                               "--lmd={} --lmd_sem=0.1 --model='CLF4SRec' --contrast='us_x' --sim={} "
-                              "--tau=1 --hidden_dropout_prob={} --attn_dropout_prob={} --lmd_tf={}".format(dataset,lmd, sim, drop, drop, lmd_tf))
+                              "--tau=1 --hidden_dropout_prob={} --attn_dropout_prob={} --lmd_tf={}".format(data,lmd, sim, drop, drop, lmd_tf))
